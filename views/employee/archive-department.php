2ea78c374d438b54b82f9dc0935821fecbe3c7c2
jc_mzhirov <mzhirov@justcoded.co>
2017-12-19 15:59:12 +0200

    added in templates Post and Term field data receiving

--- views/employee/archive-department.php
+++ views/employee/archive-department.php
@@ -3,7 +3,12 @@
  * The template for displaying custom taxonomy pages.
  */
 
+use JustCoded\WP\Framework\Objects\Termmeta;
+
 /* @var \JustCoded\WP\Framework\Web\View $this */
+
+// get custom fields data
+$fields = new Termmeta();
 
 $this->extends( 'layouts/main' ); ?>
 
@@ -15,6 +20,19 @@ $this->extends( 'layouts/main' ); ?>
 				the_archive_description( '<div class="taxonomy-description">', '</div>' );
 			?>
 		</header><!-- .page-header -->
+
+		<div>
+			<?php
+			// print custom field department, if it exists
+			if ( $fields->department ) : ?>
+				<h3><?php echo $fields->department; ?></h3>
+			<?php endif; ?>
+			<?php
+			// print custom field floor, if it exists
+			if ( $fields->floor ) : ?>
+				<h3><?php echo $fields->floor; ?></h3>
+			<?php endif; ?>
+		</div>
 
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
--- views/employee/single.php
+++ views/employee/single.php
@@ -3,11 +3,28 @@
  * The template for displaying CPT Example
  */
 
+use JustCoded\WP\Framework\Objects\Postmeta;
+
 /* @var \JustCoded\WP\Framework\Web\View $this */
+
+// get custom fields data
+$fields = new Postmeta();
 
 $this->extends( 'layouts/main' ); ?>
 
 	<h1><?php the_title(); ?></h1>
+	<div>
+		<?php
+		// print custom field position, if it exists
+		if ( $fields->position ) : ?>
+		<h3><?php echo $fields->position; ?></h3>
+		<?php endif; ?>
+		<?php
+		// print custom field bio, if it exists
+		if ( $fields->bio ) : ?>
+			<h3><?php echo $fields->bio; ?></h3>
+		<?php endif; ?>
+	</div>
 	<?php while ( have_posts() ) : the_post(); ?>
 
 		<?php $this->render( 'employee/_content', array(
