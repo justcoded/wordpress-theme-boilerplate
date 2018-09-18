91217a786e0d86dfd732899ed2bb04f5149eb5df
Alex Prokopenko <ab.prokopenko@gmail.com>
2018-09-18 18:48:35 +0300

    Fix ACF Theme fields, requirements print

--- requirements.php
+++ requirements.php
@@ -67,7 +67,7 @@ if ( ! class_exists( 'Just_Theme_Framework_Checker' ) ) {
 		private function __construct() {
 			global $pagenow;
 			if ( ! is_admin() && 'wp-login.php' !== $pagenow && ! $this->check_requirements() ) {
-				throw new Exception( 'Your theme requires Just Theme Framework and Titan Framework plugins to be installed and activated.' );
+				throw new Exception( 'Your theme requires WordPress Theme Framework and Advanced Custom Fields PRO plugins to be installed and activated.' );
 			}
 			add_action( 'admin_notices', array( $this, 'display_requirements_admin_notice' ) );
 		}
@@ -127,7 +127,17 @@ if ( ! class_exists( 'Just_Theme_Framework_Checker' ) ) {
 			}
 
 			$html = '<div class="error"><h3>Please fix the errors below to use current activated theme:</h3><p>' . implode( '</p><p>', $warnings ) . '</p></div>';
-			echo wp_kses( $html, array( 'div', 'h3', 'p' ) );
+			echo wp_kses( $html, array(
+				'div' => [
+					'class' => true,
+				],
+				'h3'  => true,
+				'p'   => true,
+				'a'   => [
+					'href'   => true,
+					'target' => true,
+				],
+			) );
 		}
 	}
 
