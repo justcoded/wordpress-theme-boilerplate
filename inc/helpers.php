0b4092519dfe629268a8d9e2ee58353fed52ff53
Nikita Dudarev <ndudarev@justcoded.co>
2019-05-01 13:42:19 +0300

    updating theme boilerplate for WP 5.2

--- inc/helpers.php
+++ inc/helpers.php
@@ -5,3 +5,11 @@
  * For example, it can be some kind of date formatters, array sorters, etc.
  */
 
+/**
+ * Support previous versions of WordPress.
+ */
+if ( ! function_exists( 'wp_body_open' ) ) {
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
